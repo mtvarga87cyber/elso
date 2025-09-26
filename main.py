4fadfd35eb7c44e3ce026a7d285b7064667ab331
E4AUTA <mt.varga87@gmail.com>
2025-09-26 20:03:51 +0200

    teszt negáció

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('teszt VM')
+    print_hi('VM')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
