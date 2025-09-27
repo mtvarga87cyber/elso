a2f2f6ee69f9da2f57d4555283f25757c74ab403
E4AUTA <mt.varga87@gmail.com>
2025-09-27 10:25:12 +0200

    p02

--- p02.py
+++ p02.py
@@ -1,13 +1,66 @@
 # 2. alkalom
-from curses.ascii import isdigit
 
-szam = (input("Kérek egy számot:"))
-if isdigit(szam):
+import random
+
+'''szam = (input("Kérek egy számot:"))
+if szam.isdigit():
     szam = int(szam)
-
-if szam > 0:
-    print("Pozitív")
-elif szam < 0:
-    print("Negatív")
+    if szam > 0:
+        print("Pozitív")
+    elif szam < 0:
+        print("Negatív")
+    else:
+        print("Nulla")
 else:
-    print("Nulla")
+    print("Nem számot adtál meg")
+
+jegy = int(input("Kérek egy érdemjegyet:"))
+while jegy == 1 or jegy > 5 or jegy < 1:
+    jegy = int(input("Kérek egy érdemjegyet:"))
+    if jegy == 10:
+        print("Csillagos ötös!")
+        break
+else:
+    print("Jól teljesített")
+print("Gratulálunk!")
+
+
+
+kocka = random.randint(1, 6)
+while True:
+    tipp = int(input("Kérek egy tippet:"))
+    if tipp == kocka:
+        break
+print("Sikerült eltalálnod a számot!")
+
+
+def fv():
+    pass
+
+for elem in range(1, 6):
+    if elem == 3:
+        continue
+    print(elem, "Nem leszek többet rossz!")
+
+try:
+    print(10/szam)
+except ZeroDivisionError:
+    print("HIBA! Nullával osztás!")
+except NameError:
+    print("HIBA! Névhiba!")
+print("OK")'''
+
+
+while True:
+    try:
+        szam = int(input("Kérek egy egész számot:"))
+    except:
+        print("Nem egész számot adott meg!")
+    else:
+        break
+
+
+
+
+
+
